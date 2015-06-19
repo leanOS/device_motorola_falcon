207d3fd9b834da084e531308c44e317b50210e8c
doublecup <doublecup@yopmail.com>
2015-06-19 11:56:31 -0400

    initial falcon tree

--- lean.mk
+++ lean.mk
@@ -1,5 +1,5 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/lean/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := slim_falcon
+PRODUCT_NAME := lean_falcon
